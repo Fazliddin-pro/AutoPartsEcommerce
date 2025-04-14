00512bbadcbe080888f6af21d7b5af81e09ffeb1
Fazliddin <sarofiddinovfazliddin@gmail.com>
2025-04-14 21:13:33 +0500

    APIs for Carts, Orders and their Items

--- sales/urls.py
+++ sales/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CartRetrieveView, CartItemViewSet
+from .views import CartRetrieveView, CartItemViewSet, OrderViewSet
 
-router = DefaultRouter()
-router.register(r'items', CartItemViewSet, basename='cartitem')
+cart_router = DefaultRouter()
+cart_router.register(r'items', CartItemViewSet, basename='cartitem')
+
+main_router = DefaultRouter()
+main_router.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [
-    path('carts/', CartRetrieveView.as_view(), name='cart-detail'),
-    path('carts/', include(router.urls)),
+    path('carts/detail/', CartRetrieveView.as_view(), name='cart-detail'),
+    path('carts/', include(cart_router.urls)),
+    path('', include(main_router.urls)),
 ]
